57ffe17ae046e4b2d808f9bc491f095a532a5d8b
Matheus Augusto <rm87364@estudante.fieb.edu.br>
2023-08-23 18:41:15 -0300

    Crianção do ambiente de desenvolvimento

---
+++ src/main/java/com/hoppe/HOOPEtcc/HoopEtccApplication.java
@@ -0,0 +1,13 @@
+package com.hoppe.HOOPEtcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoopEtccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoopEtccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoppe/HOOPEtcc/HoopEtccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoppe.HOOPEtcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoopEtccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
